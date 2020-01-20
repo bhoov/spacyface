ec253ecbc0084503efa7ae99a8313dacc9e5885f
Benjamin Hoover <benjamin.hoover@ibm.com>
2020-01-20 15:14:51 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,12 +1,29 @@
 from setuptools import setup, find_packages
 
-requires = [] # Let conda handle requires
+requires = [
+    'transformers>=2.3.0',
+    'h5py>=2.10.0',
+    'numpy>=1.17.4',
+    'regex>=2020.1.8',
+    'torch>=1.3.1',
+]
 
 setup(
     name="spacyface",
     description="Aligner for spacy and huggingface tokenization",
-    packages=find_packages(),
+    packages=['spacyface'],
+    version='0.1',
+    license='MIT',
     author="Ben Hoover",
+    author_email="benjamin.hoover@ibm.com",
+    url="https://github.com/bhoov/spacyface",
+    keywords=["transformer", "pytorch", "spacy", "tokenize", "tokenization", "NLP", "Natural Language Processing",
+              "huggingface", "linguistic"],
     include_package_data=True,
-    install_requires=requires
+    install_requires=requires,
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        ]
 )
